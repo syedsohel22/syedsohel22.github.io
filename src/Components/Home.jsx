8b8d996781de0b1662f0054bd8b8f1b2894c27f1
sohel <mr.sohelsyed@gmail.com>
2023-02-13 21:58:09 +0530

    added files

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,30 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Img } from "@chakra-ui/react";
 import React from "react";
 
 import "./home.css";
+
 const Home = () => {
   return (
     <Box id="home" h="100vh">
-      <Heading id="user-detail-name" fontSize={80} fontFamily={"old english"}>
-        Sohel Syed
-      </Heading>
-      <Heading id="user-detail-intro">A Full Stack Developer.!</Heading>
+      <Flex align="center" justify="center" gap={10} mt={10}>
+        <Box>
+          <Heading
+            id="user-detail-name"
+            fontSize={80}
+            fontFamily={"old english"}
+          >
+            Sohel Syed
+          </Heading>
+          <Heading id="user-detail-intro">A Full Stack Developer.!</Heading>
+        </Box>
+
+        <Img
+          className="home-img"
+          src="https://avatars.githubusercontent.com/u/83800834?v=4"
+          alt="sohel-syed"
+          borderRadius="50%"
+        />
+      </Flex>
     </Box>
   );
 };
