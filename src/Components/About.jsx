452a49cc29e62830ea6d4879ee125bc0b6cf4c2c
sohel <mr.sohelsyed@gmail.com>
2023-06-08 12:09:41 +0530

    resolving the download button

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,10 +9,14 @@ const About = () => {
       <Text id="user-detail-name">Sohel Syed</Text>
       <Text id="user-detail-intro">
         <span> Hello my Name is Sohel Syed I am a Full Stack Developer</span>
-        An enthusiastic Full-stack Developer, skilled in React.js. Developing
-        websites using JavaScript, Node.js, Express.js, MongoDB. An optimistic
-        man, always ready to take a critical decision, ready to work under
-        pressure. Always ready to try some new things for optimization.
+        <br />
+        An enthusiastic Full-stack Developer, skilled in React.js. Developing{" "}
+        <br />
+        websites using JavaScript, Node.js, Express.js, MongoDB. An optimistic{" "}
+        <br />
+        man, always ready to take a critical decision, ready to work under{" "}
+        <br />
+        pressure. Always ready to try some new things for optimization. <br />
       </Text>
       <Text></Text>
       <Button id="resume-button-2">
@@ -23,7 +27,8 @@ const About = () => {
           download="Sohel-Syed-Resume.pdf"
           onClick={() => {
             window.open(
-              "https://drive.google.com/file/d/1BQS12moCHECzWVibbiqGVVUZzCxFzvMS/view?usp=share_link"
+              "https://drive.google.com/file/d/1BQS12moCHECzWVibbiqGVVUZzCxFzvMS/view?usp=share_link",
+              "_blank"
             );
           }}
         >
