8f6f6e98a60eb014989bff46a1cac3fc04890840
sohel <syed.sohel@learnbay.co>
2026-09-17 17:38:43 +0530

    updated

--- src/Components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import { Center } from "@chakra-ui/react";
-import React from "react";
-
-const Footer = () => {
-  return <Center p={10}>Build with ❤️ by Sohel Syed.</Center>;
-};
-
-export default Footer;
